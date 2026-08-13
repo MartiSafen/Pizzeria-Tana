09a970ada76aef44702216e1a3c7763c8d375ae0
martina <martinasafenreiter@hotmail.com>
2026-08-13 19:51:47 -0300

    cambios finales

--- src/app/pizza-list/pizza-list.ts
+++ src/app/pizza-list/pizza-list.ts
@@ -1,10 +1,11 @@
-import { Component,OnInit } from '@angular/core';
-import {Pizza } from './pizza';  
+import { Component, OnInit, signal } from '@angular/core';
+import { Pizza } from './pizza';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { InputInteger } from '../input-integer/input-integer';
-import { PizzaCartService} from '../pizza-cart';
+import { PizzaCartService } from '../pizza-cart';
 import { PizzaData } from '../pizza-data';
+
 @Component({
   selector: 'app-pizza-list',
   standalone: true,
@@ -13,26 +14,26 @@ import { PizzaData } from '../pizza-data';
   styleUrl: './pizza-list.css',
 })
 export class PizzaList implements OnInit {
-pizzas: Pizza[] = [];
 
-constructor(private pizzaData: PizzaData, private cart: PizzaCartService) {}
-ngOnInit(): void {
-  this.pizzaData.getAll().subscribe(pizzas  => this.pizzas =pizzas); 
-}
+  pizzas = signal<Pizza[]>([]);
 
+  constructor(private pizzaData: PizzaData, private cart: PizzaCartService) {}
 
-addToCart(pizza: Pizza): void {
-  if (pizza.quantity <= 0) {
-    return;
+  ngOnInit(): void {
+    this.pizzaData.getAll().subscribe(pizzas => this.pizzas.set(pizzas));
   }
 
-  this.cart.addToCart(pizza);
-  pizza.stock -= pizza.quantity;
-  pizza.quantity = 0;
-}
- maxReached(message: string) {
-  console.log(message);
-}
- 
+  addToCart(pizza: Pizza): void {
+    if (pizza.quantity <= 0) {
+      return;
+    }
 
+    this.cart.addToCart(pizza);
+    pizza.stock -= pizza.quantity;
+    pizza.quantity = 0;
+  }
+
+  maxReached(message: string) {
+    console.log(message);
+  }
 }
